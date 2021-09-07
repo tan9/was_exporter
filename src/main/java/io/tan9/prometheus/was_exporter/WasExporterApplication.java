362e044a13b28ca41a8647f61e0374a56aa99bad
Pei-Tang Huang <tangtheone@gmail.com>
2021-09-07 11:08:50 +0800

    Init.

---
+++ src/main/java/io/tan9/prometheus/was_exporter/WasExporterApplication.java
@@ -0,0 +1,13 @@
+package io.tan9.prometheus.was_exporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasExporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/tan9/prometheus/was_exporter/WasExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tan9.prometheus.was_exporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasExporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
